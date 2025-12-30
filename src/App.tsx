bec1d2d53a19beedbc09d2aa5cf9a522b775e65b
isaac-wahba <isaac.wahba2511@gmail.com>
2025-12-30 19:27:35 +0200

    NB-00: add lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,44 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Box, CircularProgress } from "@mui/material";
 import { Layout } from "./components/Layout";
-import { Home } from "./pages/Home";
-import { Application } from "./pages/Application";
 import "./i18n";
+
+const Home = lazy(() =>
+  import("./pages/Home").then((module) => ({ default: module.Home }))
+);
+const Application = lazy(() =>
+  import("./pages/Application").then((module) => ({
+    default: module.Application,
+  }))
+);
+
+function LoadingFallback() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "calc(100vh - 64px)",
+      }}
+    >
+      <CircularProgress />
+    </Box>
+  );
+}
 
 function App() {
   return (
     <BrowserRouter>
       <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/application" element={<Application />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
+        <Suspense fallback={<LoadingFallback />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/application" element={<Application />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </Suspense>
       </Layout>
     </BrowserRouter>
   );
